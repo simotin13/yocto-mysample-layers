1157b11e394893a4f0d93284753e496faf40b0d8
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2019-12-05 15:15:12 +0900

    delete unused dirs.

--- meta-calc/recipes-calc/include/libcalc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LIBCALC_H_
-#define _LIBCALC_H_
-
-extern int add(int a, int b);
-extern int sub(int a, int b);
-extern int mul(int a, int b);
-extern int div(int a, int b);
-
-#endif	// _LIBCALC_H_
